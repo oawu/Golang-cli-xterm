5fab5210b9ce2b97071396585b724b1cd8496e6b
OA Wu <oawu.tw@gmail.com>
2022-08-23 17:43:47 +0800

    修正

--- xterm.go
+++ xterm.go
@@ -11,6 +11,14 @@ import (
 	"fmt"
 )
 
+func light(num uint8, lights ...bool) uint8 {
+	if len(lights) > 0 && lights[0] {
+		return num + 8
+	} else {
+		return num
+	}
+}
+
 type Str struct {
 	val   string
 	codes []string
@@ -35,6 +43,60 @@ func (str *Str) Color(code uint8) *Str      { return str.code(fmt.Sprintf("\x1b[
 func (str *Str) Background(code uint8) *Str { return str.code(fmt.Sprintf("\x1b[48;5;%dm", code)) }
 func (str *Str) Bg(code uint8) *Str         { return str.Background(code) }
 
+func (str *Str) Black(lights ...bool) *Str  { return str.Color(light(0, lights...)) }
+func (str *Str) Red(lights ...bool) *Str    { return str.Color(light(1, lights...)) }
+func (str *Str) Green(lights ...bool) *Str  { return str.Color(light(2, lights...)) }
+func (str *Str) Yellow(lights ...bool) *Str { return str.Color(light(3, lights...)) }
+func (str *Str) Blue(lights ...bool) *Str   { return str.Color(light(4, lights...)) }
+func (str *Str) Purple(lights ...bool) *Str { return str.Color(light(5, lights...)) }
+func (str *Str) Cyan(lights ...bool) *Str   { return str.Color(light(6, lights...)) }
+func (str *Str) Gray(lights ...bool) *Str   { return str.Color(light(7, lights...)) }
+
+func (str *Str) LightBlack() *Str  { return str.Color(light(0, true)) }
+func (str *Str) LightRed() *Str    { return str.Color(light(1, true)) }
+func (str *Str) LightGreen() *Str  { return str.Color(light(2, true)) }
+func (str *Str) LightYellow() *Str { return str.Color(light(3, true)) }
+func (str *Str) LightBlue() *Str   { return str.Color(light(4, true)) }
+func (str *Str) LightPurple() *Str { return str.Color(light(5, true)) }
+func (str *Str) LightCyan() *Str   { return str.Color(light(6, true)) }
+func (str *Str) LightGray() *Str   { return str.Color(light(7, true)) }
+
+func (str *Str) BackgroundBlack(lights ...bool) *Str  { return str.Background(light(0, lights...)) }
+func (str *Str) BackgroundRed(lights ...bool) *Str    { return str.Background(light(1, lights...)) }
+func (str *Str) BackgroundGreen(lights ...bool) *Str  { return str.Background(light(2, lights...)) }
+func (str *Str) BackgroundYellow(lights ...bool) *Str { return str.Background(light(3, lights...)) }
+func (str *Str) BackgroundBlue(lights ...bool) *Str   { return str.Background(light(4, lights...)) }
+func (str *Str) BackgroundPurple(lights ...bool) *Str { return str.Background(light(5, lights...)) }
+func (str *Str) BackgroundCyan(lights ...bool) *Str   { return str.Background(light(6, lights...)) }
+func (str *Str) BackgroundGray(lights ...bool) *Str   { return str.Background(light(7, lights...)) }
+
+func (str *Str) BackgroundLightBlack() *Str  { return str.Background(light(0, true)) }
+func (str *Str) BackgroundLightRed() *Str    { return str.Background(light(1, true)) }
+func (str *Str) BackgroundLightGreen() *Str  { return str.Background(light(2, true)) }
+func (str *Str) BackgroundLightYellow() *Str { return str.Background(light(3, true)) }
+func (str *Str) BackgroundLightBlue() *Str   { return str.Background(light(4, true)) }
+func (str *Str) BackgroundLightPurple() *Str { return str.Background(light(5, true)) }
+func (str *Str) BackgroundLightCyan() *Str   { return str.Background(light(6, true)) }
+func (str *Str) BackgroundLightGray() *Str   { return str.Background(light(7, true)) }
+
+func (str *Str) BgBlack(lights ...bool) *Str  { return str.Background(light(0, lights...)) }
+func (str *Str) BgRed(lights ...bool) *Str    { return str.Background(light(1, lights...)) }
+func (str *Str) BgGreen(lights ...bool) *Str  { return str.Background(light(2, lights...)) }
+func (str *Str) BgYellow(lights ...bool) *Str { return str.Background(light(3, lights...)) }
+func (str *Str) BgBlue(lights ...bool) *Str   { return str.Background(light(4, lights...)) }
+func (str *Str) BgPurple(lights ...bool) *Str { return str.Background(light(5, lights...)) }
+func (str *Str) BgCyan(lights ...bool) *Str   { return str.Background(light(6, lights...)) }
+func (str *Str) BgGray(lights ...bool) *Str   { return str.Background(light(7, lights...)) }
+
+func (str *Str) BgLightBlack() *Str  { return str.Background(light(0, true)) }
+func (str *Str) BgLightRed() *Str    { return str.Background(light(1, true)) }
+func (str *Str) BgLightGreen() *Str  { return str.Background(light(2, true)) }
+func (str *Str) BgLightYellow() *Str { return str.Background(light(3, true)) }
+func (str *Str) BgLightBlue() *Str   { return str.Background(light(4, true)) }
+func (str *Str) BgLightPurple() *Str { return str.Background(light(5, true)) }
+func (str *Str) BgLightCyan() *Str   { return str.Background(light(6, true)) }
+func (str *Str) BgLightGray() *Str   { return str.Background(light(7, true)) }
+
 func (str *Str) String() string {
 	tmp := str.val
 	for _, code := range str.codes {
@@ -45,6 +107,7 @@ func (str *Str) String() string {
 func New(str string) *Str {
 	return &Str{val: str}
 }
+
 func Bold(str string) *Str      { return New(str).Bold() }
 func Dim(str string) *Str       { return New(str).Dim() }
 func Italic(str string) *Str    { return New(str).Italic() }
@@ -53,56 +116,56 @@ func Blink(str string) *Str     { return New(str).Blink() }
 func Inverted(str string) *Str  { return New(str).Inverted() }
 func Hidden(str string) *Str    { return New(str).Hidden() }
 
-func Black(str string) *Str  { return New(str).Color(0) }
-func Red(str string) *Str    { return New(str).Color(1) }
-func Green(str string) *Str  { return New(str).Color(2) }
-func Yellow(str string) *Str { return New(str).Color(3) }
-func Blue(str string) *Str   { return New(str).Color(4) }
-func Purple(str string) *Str { return New(str).Color(5) }
-func Cyan(str string) *Str   { return New(str).Color(6) }
-func Gray(str string) *Str   { return New(str).Color(7) }
+func Black(str string, lights ...bool) *Str  { return New(str).Black(lights...) }
+func Red(str string, lights ...bool) *Str    { return New(str).Red(lights...) }
+func Green(str string, lights ...bool) *Str  { return New(str).Green(lights...) }
+func Yellow(str string, lights ...bool) *Str { return New(str).Yellow(lights...) }
+func Blue(str string, lights ...bool) *Str   { return New(str).Blue(lights...) }
+func Purple(str string, lights ...bool) *Str { return New(str).Purple(lights...) }
+func Cyan(str string, lights ...bool) *Str   { return New(str).Cyan(lights...) }
+func Gray(str string, lights ...bool) *Str   { return New(str).Gray(lights...) }
 
-func LightBlack(str string) *Str  { return New(str).Color(8) }
-func LightRed(str string) *Str    { return New(str).Color(9) }
-func LightGreen(str string) *Str  { return New(str).Color(10) }
-func LightYellow(str string) *Str { return New(str).Color(11) }
-func LightBlue(str string) *Str   { return New(str).Color(12) }
-func LightPurple(str string) *Str { return New(str).Color(13) }
-func LightCyan(str string) *Str   { return New(str).Color(14) }
-func LightGray(str string) *Str   { return New(str).Color(15) }
+func BackgroundBlack(str string, lights ...bool) *Str  { return New(str).BackgroundBlack(lights...) }
+func BackgroundRed(str string, lights ...bool) *Str    { return New(str).BackgroundRed(lights...) }
+func BackgroundGreen(str string, lights ...bool) *Str  { return New(str).BackgroundGreen(lights...) }
+func BackgroundYellow(str string, lights ...bool) *Str { return New(str).BackgroundYellow(lights...) }
+func BackgroundBlue(str string, lights ...bool) *Str   { return New(str).BackgroundBlue(lights...) }
+func BackgroundPurple(str string, lights ...bool) *Str { return New(str).BackgroundPurple(lights...) }
+func BackgroundCyan(str string, lights ...bool) *Str   { return New(str).BackgroundCyan(lights...) }
+func BackgroundGray(str string, lights ...bool) *Str   { return New(str).BackgroundGray(lights...) }
 
-func BackgroundBlack(str string) *Str  { return New(str).Background(0) }
-func BackgroundRed(str string) *Str    { return New(str).Background(1) }
-func BackgroundGreen(str string) *Str  { return New(str).Background(2) }
-func BackgroundYellow(str string) *Str { return New(str).Background(3) }
-func BackgroundBlue(str string) *Str   { return New(str).Background(4) }
-func BackgroundPurple(str string) *Str { return New(str).Background(5) }
-func BackgroundCyan(str string) *Str   { return New(str).Background(6) }
-func BackgroundGray(str string) *Str   { return New(str).Background(7) }
+func BgBlack(str string, lights ...bool) *Str  { return New(str).BackgroundBlack(lights...) }
+func BgRed(str string, lights ...bool) *Str    { return New(str).BackgroundRed(lights...) }
+func BgGreen(str string, lights ...bool) *Str  { return New(str).BackgroundGreen(lights...) }
+func BgYellow(str string, lights ...bool) *Str { return New(str).BackgroundYellow(lights...) }
+func BgBlue(str string, lights ...bool) *Str   { return New(str).BackgroundBlue(lights...) }
+func BgPurple(str string, lights ...bool) *Str { return New(str).BackgroundPurple(lights...) }
+func BgCyan(str string, lights ...bool) *Str   { return New(str).BackgroundCyan(lights...) }
+func BgGray(str string, lights ...bool) *Str   { return New(str).BackgroundGray(lights...) }
 
-func BgBlack(str string) *Str  { return New(str).Background(0) }
-func BgRed(str string) *Str    { return New(str).Background(1) }
-func BgGreen(str string) *Str  { return New(str).Background(2) }
-func BgYellow(str string) *Str { return New(str).Background(3) }
-func BgBlue(str string) *Str   { return New(str).Background(4) }
-func BgPurple(str string) *Str { return New(str).Background(5) }
-func BgCyan(str string) *Str   { return New(str).Background(6) }
-func BgGray(str string) *Str   { return New(str).Background(7) }
+func LightBlack(str string) *Str  { return New(str).LightBlack() }
+func LightRed(str string) *Str    { return New(str).LightRed() }
+func LightGreen(str string) *Str  { return New(str).LightGreen() }
+func LightYellow(str string) *Str { return New(str).LightYellow() }
+func LightBlue(str string) *Str   { return New(str).LightBlue() }
+func LightPurple(str string) *Str { return New(str).LightPurple() }
+func LightCyan(str string) *Str   { return New(str).LightCyan() }
+func LightGray(str string) *Str   { return New(str).LightGray() }
 
-func BackgroundLightBlack(str string) *Str  { return New(str).Background(8) }
-func BackgroundLightRed(str string) *Str    { return New(str).Background(9) }
-func BackgroundLightGreen(str string) *Str  { return New(str).Background(10) }
-func BackgroundLightYellow(str string) *Str { return New(str).Background(11) }
-func BackgroundLightBlue(str string) *Str   { return New(str).Background(12) }
-func BackgroundLightPurple(str string) *Str { return New(str).Background(13) }
-func BackgroundLightCyan(str string) *Str   { return New(str).Background(14) }
-func BackgroundLightGray(str string) *Str   { return New(str).Background(15) }
+func BackgroundLightBlack(str string) *Str  { return New(str).BackgroundLightBlack() }
+func BackgroundLightRed(str string) *Str    { return New(str).BackgroundLightRed() }
+func BackgroundLightGreen(str string) *Str  { return New(str).BackgroundLightGreen() }
+func BackgroundLightYellow(str string) *Str { return New(str).BackgroundLightYellow() }
+func BackgroundLightBlue(str string) *Str   { return New(str).BackgroundLightBlue() }
+func BackgroundLightPurple(str string) *Str { return New(str).BackgroundLightPurple() }
+func BackgroundLightCyan(str string) *Str   { return New(str).BackgroundLightCyan() }
+func BackgroundLightGray(str string) *Str   { return New(str).BackgroundLightGray() }
 
-func BgLightBlack(str string) *Str  { return New(str).Background(8) }
-func BgLightRed(str string) *Str    { return New(str).Background(9) }
-func BgLightGreen(str string) *Str  { return New(str).Background(10) }
-func BgLightYellow(str string) *Str { return New(str).Background(11) }
-func BgLightBlue(str string) *Str   { return New(str).Background(12) }
-func BgLightPurple(str string) *Str { return New(str).Background(13) }
-func BgLightCyan(str string) *Str   { return New(str).Background(14) }
-func BgLightGray(str string) *Str   { return New(str).Background(15) }
+func BgLightBlack(str string) *Str  { return New(str).BgLightBlack() }
+func BgLightRed(str string) *Str    { return New(str).BgLightRed() }
+func BgLightGreen(str string) *Str  { return New(str).BgLightGreen() }
+func BgLightYellow(str string) *Str { return New(str).BgLightYellow() }
+func BgLightBlue(str string) *Str   { return New(str).BgLightBlue() }
+func BgLightPurple(str string) *Str { return New(str).BgLightPurple() }
+func BgLightCyan(str string) *Str   { return New(str).BgLightCyan() }
+func BgLightGray(str string) *Str   { return New(str).BgLightGray() }
